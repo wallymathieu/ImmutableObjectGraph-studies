6b55170623352c5ddf47a2534f81b6323080dc3c
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-09-06 07:55:45 +0200

    Partial commit

--- src/Core/Domain/Order.cs
+++ src/Core/Domain/Order.cs
@@ -1,22 +1,10 @@
-﻿using ImmutableObjectGraph.Generation;
-using System;
+﻿using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Collections.Generic;
 namespace SomeBasicFileStoreApp.Core
 {
-    [GenerateImmutable(DefineWithMethodsPerProperty = true, GenerateBuilder = true)]
-    public partial class Order
+    public record Order(int Id, int Customer, DateTime OrderDate, IEnumerable<Product> Products,  int Version)
     {
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        readonly int id;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        readonly int customer;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        readonly DateTime orderDate;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        readonly IEnumerable<Product> products;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        readonly int version;
     }
 }
